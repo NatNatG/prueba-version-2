198f0ee6741e80afc62df3d60cf9cf6243662308
danquin07 <danielqf1o@gmail.com>
2021-11-30 10:04:00 -0600

    boss a medias

---
+++ Assets/Scripts/BossControlador.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Animator))]
+public class BossControlador : MonoBehaviour
+{
+    public int life;
+    //public GameObject LaserPrefab;
+    public Transform LaserMuzzle;
+
+    private Slider HPSlider;
+
+    //private Animator BossAnimator;
+    public GameObject BossHPSlider;
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+        //BossAnimator = GetComponent<Animator>();
+        HPSlider = BossHPSlider.GetComponent<Slider>();
+
+        HPSlider.value = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerLaser"))
+        {
+            DamageBoss(2);
+        }
+    }
+
+    //animacion de daño y slider 
+    public void DamageBoss(int damage)
+    {
+        life -= (damage);
+        //BossAnimator.SetTrigger("hit");
+
+
+        //aqui baja la vida
+        HPSlider.value = life / 100.0f;
+    }
+}
+
